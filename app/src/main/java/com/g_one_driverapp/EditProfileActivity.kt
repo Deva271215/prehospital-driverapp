4a0c4b4571c098c98224a3eee4b5c3016880e9c8
Deva271215 <devaputra78@gmail.com>
2021-05-17 20:29:08 +0800

    This is driver app interface

---
+++ app/src/main/java/com/g_one_driverapp/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.g_one_driverapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
