685021068a65791ab70c2abd2778716601fb40db
Deva271215 <devaputra78@gmail.com>
2021-05-20 11:44:50 +0800

    Add redirect from sign-up to login

--- app/src/main/java/com/g_one_driverapp/SignupActivity.kt
+++ app/src/main/java/com/g_one_driverapp/SignupActivity.kt
@@ -1,11 +1,34 @@
 package com.g_one_driverapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.g_one_driverapp.databinding.ActivitySignupBinding
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onSignUpButtonClicked()
+        onSignInTextClicked()
+    }
+
+    private fun onSignUpButtonClicked() {
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun onSignInTextClicked() {
+        btnToSignIn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
