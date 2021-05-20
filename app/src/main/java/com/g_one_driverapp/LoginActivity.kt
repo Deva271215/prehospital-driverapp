bc27b3b58597b7d785fbe7446c92dca5821a400b
Deva271215 <devaputra78@gmail.com>
2021-05-20 11:35:39 +0800

    Redirect from login activity to main activity

--- app/src/main/java/com/g_one_driverapp/LoginActivity.kt
+++ app/src/main/java/com/g_one_driverapp/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.g_one_driverapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.g_one_driverapp.databinding.ActivityLoginBinding
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onSignInButtonClicked()
+    }
+
+    private fun onSignInButtonClicked() {
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
