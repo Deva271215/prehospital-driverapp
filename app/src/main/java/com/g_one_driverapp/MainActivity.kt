cdc46492af752dc268964297f94e5d31adea02aa
IrvanPrama <bluxfusion@gmail.com>
2021-06-04 00:06:36 +0800

    Added intent to google maps

--- app/src/main/java/com/g_one_driverapp/MainActivity.kt
+++ app/src/main/java/com/g_one_driverapp/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.g_one_driverapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.g_one_driverapp.databinding.ActivityMainBinding
 import com.g_one_driverapp.preferences.UserPreference
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private lateinit var preference: UserPreference
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         preference = UserPreference(applicationContext)
+        setContentView(binding.root)
 
+        val groupName = preference.getLoginData().user?.group_name
+        binding.titleUName.text = "Driver $groupName"
+
+        sendCoordinatetoMaps()
         subscribeToFirebaseTopic()
+    }
+
+    private fun sendCoordinatetoMaps() {
+        binding.openMaps.setOnClickListener {
+            val gmmIntentUri = Uri.parse("google.navigation:q=-8.703472,115.248977")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
     }
 
     private fun subscribeToFirebaseTopic() {
@@ -26,7 +43,6 @@ class MainActivity : AppCompatActivity() {
             if (!it.isSuccessful) {
                 message = "Failed to subscribe to $driverId!"
             }
-            Log.i("hospital_code", driverId!!)
             Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
         }
     }
